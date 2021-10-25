da41b23cec6a6f2f21eb5e0d8a6be3a2e5ab5742
Andretreccia <andreabtreccia@gmail.com>
2021-10-25 12:36:03 +0200

    Snack2_b

--- snack_2/main.js
+++ snack_2/main.js
@@ -20,3 +20,13 @@ let squadre = [
         falliSubiti: 0
     }
 ]
+//generare numeri random al posto degli 0 nelle pripietà: punti fatti e falli subiti
+
+// Math.floor(Math.random() * (max - min + 1)) + min;
+
+for (i = 0; i < squadre.length; i++) {
+    let squadra = squadre[i]
+    squadra.puntiFatti = Math.floor(Math.random() * (100 - 0 + 1)) + 1;
+    squadra.falliSubiti = Math.floor(Math.random() * (100 - 0 + 1)) + 1;
+    console.log(squadra)
+}
