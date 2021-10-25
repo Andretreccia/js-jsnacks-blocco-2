e462838d93573d0049d3c57c8e14ffc538e2a4e2
Andretreccia <andreabtreccia@gmail.com>
2021-10-25 12:22:03 +0200

    snack_2

---
+++ snack_2/main.js
@@ -0,0 +1,22 @@
+let squadre = [
+    {
+        name: 'Lazio',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        name: 'Inter',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        name: 'Roma',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        name: 'Napoli',
+        puntiFatti: 0,
+        falliSubiti: 0
+    }
+]
